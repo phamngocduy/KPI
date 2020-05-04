7bab0853687dca4d814d9e48e2c8106ab367ed4f
phamngocduy <phmngcduy@gmail.com>
2020-05-04 11:12:12 +0700

    Update Login gui and Done message icon

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -164,7 +164,7 @@ namespace WebApplication.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "KPI");
                 }
                 AddErrors(result);
             }
@@ -393,7 +393,7 @@ namespace WebApplication.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "KPI");
         }
 
         //
@@ -450,7 +450,7 @@ namespace WebApplication.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "KPI");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
